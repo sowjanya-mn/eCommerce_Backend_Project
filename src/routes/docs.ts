98309a3e0d9b1b4808b0d7690f2953f09f2e572e
Sowjanya Madre Nagaraj <sowjanya.mn04@gmail.com>
2026-09-16 19:01:26 +0200

    installed cors dependency

--- src/routes/docs.ts
+++ src/routes/docs.ts
@@ -1,27 +1,3 @@
-// import { Router } from "express";
-// import swaggerJSDoc, { type Options } from "swagger-jsdoc";
-// import packageJson from "../../package.json" with { type: "json" };
-
-// const docsRoutes = Router();
-
-// const options: Options = {
-//   definition: {
-//     openapi: "3.1.0",
-//     info: {
-//       title: "eCommerce API",
-//       version: packageJson.version,
-//     },
-//   },
-//   apis: ["./src/controllers/*.{ts,js}", "./src/schemas/*.{ts,js}"],
-// };
-
-// export const swaggerSpec = swaggerJSDoc(options);
-
-// docsRoutes.get("/openapi.json", (req, res) => {
-//   res.json(swaggerSpec);
-// });
-
-// export default docsRoutes;
 import { Router } from "express";
 import swaggerJSDoc, { type Options } from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
